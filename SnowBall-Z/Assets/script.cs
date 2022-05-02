5986805dd12d0153ed62c4ef8557b2bf8ee5f312
simonk00 <kaersound@gmail.com>
2022-05-02 21:51:47 +0200

    Added Network Manager

---
+++ SnowBall-Z/Assets/script.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+        transform.position += Vector3.forward * 10 * Time.deltaTime;
+    }
+}
