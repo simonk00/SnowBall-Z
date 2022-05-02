d2c0ba92c5584c8d1d1cd0e0c09163ad43ab5e7f
SimonDyrn <simondyrn@gmail.com>
2022-05-02 23:10:44 +0200

    Hej så er der ting

---
+++ SnowBall-Z/Assets/UIManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button startServerButton;
+
+    [SerializeField]
+    private Button startHostButton;
+
+    [SerializeField]
+    private Button startClientButton;
+
+    [SerializeField]
+    private TextMeshProUGUI playersInGameText;
+
+    private void Awake()
+    {
+        Cursor.visible = true;
+    }
+
+    private void Update()
+    {
+        playersInGameText.text = $"Players In Game: {PlayersManager.Instance.PlayersInGame}";
+    }
+
+    private void Start()
+    {
+        startHostButton.onClick.AddListener(() =>
+        {
+            if (NetworkManager.Singleton.StartHost())
+            {
+                Logger.Instance.LogInfo("Host Started...");
+            }
+            else
+            {
+                Logger.Instance.LogInfo("Host Not Started...");
+            }
+        });
+
+        startServerButton.onClick.AddListener(() =>
+        {
+            if (NetworkManager.Singleton.StartServer())
+            {
+                Logger.Instance.LogInfo("Server Started...");
+            }
+            else
+            {
+                Logger.Instance.LogInfo("Server Not Started...");
+            }
+        });
+
+        startClientButton.onClick.AddListener(() =>
+        {
+            if (NetworkManager.Singleton.StartClient())
+            {
+                Logger.Instance.LogInfo("Client Started...");
+            }
+            else
+            {
+                Logger.Instance.LogInfo("Client Not Started...");
+            }
+        });
+
+    }
+}
